5d45b0a2b0dae082ff35069375f092e09421eeb9
yashmouje <59363118+yashmouje@users.noreply.github.com>
2022-11-05 13:23:41 -0600

    Basic HTML Files

--- greenscore_app/urls.py
+++ greenscore_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'greenscore_app'
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('my-score/', views.scores, name='scores')
+
 ]
